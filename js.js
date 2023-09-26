27041eec1cb4f8d90592d8a014458c9499c787c0
RoseAlline <98618658+RoseAlline@users.noreply.github.com>
2023-09-26 18:31:56 +0300

    Update js.js

--- js.js
+++ js.js
@@ -3,55 +3,19 @@ import { Tile } from "./tile.js";
 
 const gameBoard = document.getElementById("game-board");
 
-const touchEvent = () => {
-  const regexp = /Android|webOS|iPhone|iPad|iPod|BlackBerry|BB|PlayBook|IEMobile|Windows Phone|Kindle|Silk|Opera Mini/i;
-  let moveStartX, moveStartY;
-
-  const start = (event) => {
-      moveStartX = event.clientX;
-      moveStartY = event.clientY;
-  }
-
-  const end = (event) => {
-      let moveEndX = event.clientX;
-      let moveEndY = event.clientY;
-
-      var dx = moveEndX - moveStartX;
-      var dy = moveEndY - moveStartY;
-
-      var absDx = Math.abs(dx);
-      var absDy = Math.abs(dy);
-
-      if (Math.max(absDx, absDy) > 10) {
-          self.input("move", absDx > absDy ? (dx > 0 ? "Right" : "Left") : (dy > 0 ? "Down" : "Up"));
-      }
-  }
-
-  gameContainer.ondragstart = () => { return false; };
-
-  setupInputOnce();
-
-}
-
 const grid = new Grid(gameBoard);
 grid.getRandomEmptyCell().linkTile(new Tile(gameBoard));
 grid.getRandomEmptyCell().linkTile(new Tile(gameBoard));
 setupInputOnce();
-touchEvent();
 
 
 function setupInputOnce() {
   window.addEventListener("keydown", handleInput, { once: true });
   window.addEventListener("mousedown", handleInput, { once: true });
   window.addEventListener("wheel", handleInput, { once: true });
-  gameContainer.addEventListener("touchstart", (event) => { start(event.touches[0]); });
-  gameContainer.addEventListener("touchend", function (event) { end(event.changedTouches[0]) });
-
-
 }
 
 window.addEventListener("contextmenu", function (e) { e.preventDefault(); }, false);
-
 
 
 
@@ -134,6 +98,49 @@ async function handleInput(event) {
 
 
   setupInputOnce();
+
+
+
+  const touchEvent = () => {
+    const regexp = /Android|webOS|iPhone|iPad|iPod|BlackBerry|BB|PlayBook|IEMobile|Windows Phone|Kindle|Silk|Opera Mini/i;
+    const gameContainer = document.querySelector(".game__container");
+    let moveStartX, moveStartY;
+
+    const start = (event) => {
+        moveStartX = event.clientX;
+        moveStartY = event.clientY;
+    }
+
+    const end = (event) => {
+        let moveEndX = event.clientX;
+        let moveEndY = event.clientY;
+
+        var dx = moveEndX - moveStartX;
+        var dy = moveEndY - moveStartY;
+
+        var absDx = Math.abs(dx);
+        var absDy = Math.abs(dy);
+
+
+       if (Math.max(absDx, absDy) > 10) {
+            self.input("move", absDx > absDy ? (dx > 0 ? "Right" : "Left") : (dy > 0 ? "Down" : "Up"));
+        }
+    }
+
+    gameContainer.ondragstart = () => { return false; };
+
+    if (regexp.test(window.navigator.userAgent)) {
+
+        gameContainer.addEventListener("touchstart", (event) => { start(event.touches[0]); });
+        gameContainer.addEventListener("touchend", function (event) { end(event.changedTouches[0]) });
+
+    } else {
+        gameContainer.addEventListener("mousedown", (event) => { start(event); });
+        gameContainer.addEventListener("mouseup", function (event) { end(event) });
+    }
+}
+
+touchEvent();
 
 }
 
