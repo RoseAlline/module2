11a761f08efaaa22545d79d17de56aba0c47864e
RoseAlline <98618658+RoseAlline@users.noreply.github.com>
2023-09-26 15:36:28 +0300

    Update js.js

--- js.js
+++ js.js
@@ -12,24 +12,6 @@ function setupInputOnce() {
   window.addEventListener("keydown", handleInput, { once: true });
   window.addEventListener("mousedown", handleInput, { once: true });
   window.addEventListener("wheel", handleInput, { once: true });
-  gameBoard.addEventListener("touchstart", e => { 
-    moveStartX = e.clientX;
-    moveStartY = e.clientY;});
-  gameBoard.addEventListener("touchend", e => { 
-((event) => { end(event.changedTouches[0]); 
-let moveEndX = event.clientX;
-    let moveEndY = event.clientY;
-
-    var dx = moveEndX - moveStartX;
-    var dy = moveEndY - moveStartY;
-
-    var absDx = Math.abs(dx);
-    var absDy = Math.abs(dy);
-
-    if (Math.max(absDx, absDy) > 10) {
-        self.input("move", absDx > absDy ? (dx > 0 ? "Right" : "Left") : (dy > 0 ? "Down" : "Up"));
-    }
-})});
 }
 
 window.addEventListener("contextmenu", function (e) { e.preventDefault(); }, false);
@@ -115,6 +97,24 @@ async function handleInput(event) {
 
 
   setupInputOnce();
+  gameBoard.addEventListener("touchstart", e => { 
+    moveStartX = e.clientX;
+    moveStartY = e.clientY;});
+  gameBoard.addEventListener("touchend", e => { 
+((event) => { end(event.changedTouches[0]); 
+let moveEndX = event.clientX;
+    let moveEndY = event.clientY;
+
+    var dx = moveEndX - moveStartX;
+    var dy = moveEndY - moveStartY;
+
+    var absDx = Math.abs(dx);
+    var absDy = Math.abs(dy);
+
+    if (Math.max(absDx, absDy) > 10) {
+        self.input("move", absDx > absDy ? (dx > 0 ? "Right" : "Left") : (dy > 0 ? "Down" : "Up"));
+    }
+})});
 
 }
 
