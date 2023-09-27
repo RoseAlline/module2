a8469cc4e9e4cc097bac9b6cb3378bba748d3860
RoseAlline <98618658+RoseAlline@users.noreply.github.com>
2023-09-27 08:39:28 +0300

    Update js.js

--- js.js
+++ js.js
@@ -2,21 +2,6 @@ import { Grid } from "./grid.js";
 import { Tile } from "./tile.js";
 
 const gameBoard = document.getElementById("game-board");
-const regexp = /Android|webOS|iPhone|iPad|iPod|BlackBerry|BB|PlayBook|IEMobile|Windows Phone|Kindle|Silk|Opera Mini/i;
-let moveStartX, moveStartY;
-
-moveStartX = event.clientX;
-moveStartY = event.clientY;
-        
-let moveEndX = event.clientX;
-let moveEndY = event.clientY;
-
-var dx = moveEndX - moveStartX;
-var dy = moveEndY - moveStartY;
-var absDx = Math.abs(dx);
-var absDy = Math.abs(dy);
-    
-
 
 const grid = new Grid(gameBoard);
 grid.getRandomEmptyCell().linkTile(new Tile(gameBoard));
@@ -28,20 +13,14 @@ function setupInputOnce() {
   window.addEventListener("keydown", handleInput, { once: true });
   window.addEventListener("mousedown", handleInput, { once: true });
   window.addEventListener("wheel", handleInput, { once: true });
-  gameBoard.addEventListener("touchstart", handleInput, { once: true });
-  gameBoard.addEventListener("touchend", handleInput, { once: true });
 }
 
 document.addEventListener('contextmenu', event => event.preventDefault());
 
-document.addEventListener('touch', event => event.preventDefault());
-window.addEventListener('scroll', event => event.preventDefault());
-
-
 
 
 async function handleInput(event) {
-  switch (event.key || event.button || event.deltaY || event.clientX || event.clientY) {
+  switch (event.key || event.button || event.deltaY ) {
     case "ArrowUp":
       if (!canMoveUp()) {
         setupInputOnce();
@@ -101,40 +80,7 @@ async function handleInput(event) {
         await moveRight();
         break;
       }
-         if (start(event.touches) > 0 & absDx > absDy) { 
-          if (!canMoveRight()) {
-          setupInputOnce();
-          return;
-        }
-        await moveRight();
-        break;
-      } else if (start(event.touches) < 0 & absDx > absDy) {
-        if (!canMoveLeft()) {
-          setupInputOnce();
-          return;
-        }
-        await moveLeft();
-        break;
-      }
-      if (start(event.touches) < 0 & absDx < absDy) {
-        if (!canMoveUp()) {
-          setupInputOnce();
-          return;
-        }
-        await moveUp();
-        break;
-      } else if (start(event.touches) > 0 & absDx < absDy) {
-        if (!canMoveDown()) {
-          setupInputOnce();
-          return;
-        }
-        await moveDown();
-        break;
-      }
-        
-      setupInputOnce();
-      return;
-      
+    
   }
 
   
