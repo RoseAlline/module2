397cfe4ca31ab36118fdfb50e965154d0c5ed0a0
RoseAlline <98618658+RoseAlline@users.noreply.github.com>
2023-09-29 08:53:07 +0300

    Update js.js

--- js.js
+++ js.js
@@ -1,192 +1,64 @@
-import { Grid } from "./grid.js";
-import { Tile } from "./tile.js";
-
-const gameBoard = document.getElementById("game-board");
-const grid = new Grid(gameBoard);
-grid.getRandomEmptyCell().linkTile(new Tile(gameBoard));
-grid.getRandomEmptyCell().linkTile(new Tile(gameBoard));
-setupInputOnce();
-
-
-function setupInputOnce() {
-  window.addEventListener("keydown", handleInput, { once: true });
-  window.addEventListener("mousedown", handleInput, { once: true });
-  window.addEventListener("wheel", handleInput, { once: true });
+* {
+    box-sizing: border-box;
+    margin: 0;
+    padding: 0;
 }
 
-document.addEventListener('contextmenu', event => event.preventDefault());
-window.addEventListener('touch', event => event.preventDefault());
+body {
+    background-color: #333;
+    justify-content: center;
+    align-items: center;
+    height: 100vh;
+    display: grid;
+    overflow: hidden;
+}
 
-async function handleInput(event) {
-  switch (event.key || event.button || event.deltaY) {
-    case "ArrowUp":
-      if (!canMoveUp()) {
-        setupInputOnce();
-        return;
-      }
-      await moveUp();
-      break;
-    case "ArrowDown":
-      if (!canMoveDown()) {
-        setupInputOnce();
-        return;
-      }
-      await moveDown();
-      break;
-    case "ArrowLeft": 
-      if (!canMoveLeft()) {
-        setupInputOnce();
-        return;
-      }
-      await moveLeft();
-      break;
-    case "ArrowRight": 
-      if (!canMoveRight()) {
-        setupInputOnce();
-        return;
-      }
-      await moveRight();
-      break;
-    default:
-      if (event.deltaY > 0) {
-        if (!canMoveUp()) {
-          setupInputOnce();
-          return;
-        }
-        await moveUp();
-        break;
-      } else if (event.deltaY < 0) {
-        if (!canMoveDown()) {
-          setupInputOnce();
-          return;
-        }
-        await moveDown();
-        break;
-      }
-      if (event.button === 0) {
-        if (!canMoveLeft()) {
-          setupInputOnce();
-          return;
-        }
-        await moveLeft();
-        break;
-      } else if (event.button === 2) {
-        if (!canMoveRight()) {
-          setupInputOnce();
-          return;
-        }
-        await moveRight();
-        break;
-      }
-       
 
-    
+
+#game-board {
+    --cell-size: 10vmin;
+    --cell-gap: 2vmin;
+    position: relative;
+    display: grid;
+    grid-template-columns: repeat(5, var(--cell-size));
+    grid-template-rows: repeat(5, var(--cell-size));
+    gap: var(--cell-gap);
+    border-radius: 1vmin;
+}
+
+.cell {
+    background-color: #eedbb0;
+    border-radius: 1vmin;
+    display: flex;
+}
+ 
+  .tile {
+    position: absolute;
+    top: calc(var(--y) * (var(--cell-size) + var(--cell-gap)));
+    left: calc(var(--x) * (var(--cell-size) + var(--cell-gap)));
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    width: var(--cell-size);
+    height: var(--cell-size);
+    border-radius: 1vmin;
+    font-family: Arial, Helvetica, sans-serif;
+    font-size: 5vmin;
+    font-weight: bold;
+    background-color: hsl(20, 60%, var(--bg-lightness));
+    color: hsl(20, 25%, var(--text-lightness));
+    animation: show 200ms;
+    transition: 100ms;
   }
-
   
-  const newTile = new Tile(gameBoard);
-  grid.getRandomEmptyCell().linkTile(newTile);
-
-  if (!canMoveUp() && !canMoveDown() && !canMoveLeft() && !canMoveRight()) {
-    await newTile.waitForAnimationEnd()
-    alert("Try again!")
-    return;
+  @keyframes show {
+    0% {
+      opacity: 0.5;
+      transform: scale(0);
+    }
   }
 
+  ::-webkit-scrollbar {
+    display: none;
+  }     
 
-  setupInputOnce();
-
-}
-
-async function moveUp() {
-  await slideTiles(grid.cellsGroupedByColumn);
-}
-
-async function moveDown() {
-  await slideTiles(grid.cellsGroupedByReversedColumn);
-}
-
-async function moveLeft() {
-  await slideTiles(grid.cellsGroupedByRow);
-}
-
-async function moveRight() {
-  await slideTiles(grid.cellsGroupedByReversedRow);
-}
-
-async function slideTiles(groupedCells) {
-  const promises = [];
-
-  groupedCells.forEach(group => slideTilesInGroup(group, promises));
-
-  await Promise.all(promises);
-  grid.cells.forEach(cell => {
-    cell.hasTileForMerge() && cell.mergeTiles()
-  });
-}
-
-function slideTilesInGroup(group, promises) {
-  for (let i = 1; i < group.length; i++) {
-    if (group[i].isEmpty()) {
-      continue;
-    }
-
-    const cellWithTile = group[i];
-
-    let targetCell;
-    let j = i - 1;
-    while (j >= 0 && group[j].canAccept(cellWithTile.linkedTile)) {
-      targetCell = group[j];
-      j--;
-    }
-
-    if (!targetCell) {
-      continue;
-    }
-
-    promises.push(cellWithTile.linkedTile.waitForTransitionEnd());
-
-    if (targetCell.isEmpty()) {
-      targetCell.linkTile(cellWithTile.linkedTile);
-    } else {
-      targetCell.linkTileForMerge(cellWithTile.linkedTile);
-    }
-
-    cellWithTile.unlinkTile();
-  }
-}
-
-function canMoveUp() {
-  return canMove(grid.cellsGroupedByColumn);
-}
-
-function canMoveDown() {
-  return canMove(grid.cellsGroupedByReversedColumn);
-}
-
-function canMoveLeft() {
-  return canMove(grid.cellsGroupedByRow);
-}
-
-function canMoveRight() {
-  return canMove(grid.cellsGroupedByReversedRow);
-}
-
-function canMove(groupedCells) {
-  return groupedCells.some(group => canMoveInGroup(group));
-}
-
-function canMoveInGroup(group) {
-  return group.some((cell, index) => {
-    if (index === 0) {
-      return false;
-    }
-
-    if (cell.isEmpty()) {
-      return false;
-    }
-
-    const targetCell = group[index - 1];
-    return targetCell.canAccept(cell.linkedTile);
-  });
-}
