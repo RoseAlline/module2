2f727f0c4721106d910360d57a95cd840d89e2e1
RoseAlline <98618658+RoseAlline@users.noreply.github.com>
2023-10-03 20:33:22 +0300

    Update js.js

--- js.js
+++ js.js
@@ -47,6 +47,15 @@ const touchEvent = () => {
 
   gameBoard.ondragstart = () => { return false; };
 
+  if (regexp.test(window.navigator.userAgent)) {
+
+    gameBoard.addEventListener("touchstart", (event) => { start(event.touches[0]); });
+    gameBoard.addEventListener("touchend", function (event) { end(event.changedTouches[0]) });
+
+} else {
+    gameBoard.addEventListener("mousedown", (event) => { start(event); });
+    gameBoard.addEventListener("mouseup", function (event) { end(event) });
+}
 
 }
 
